860bba7b0f047ec99ac34949b1abeeb6ef7b884e
EmilAvdic <e.avdic@students.htl-leonding.ac.at>
2024-12-17 15:05:57 +0100

    Frontend finished

---
+++ Frontend/BlazorWebApp/Presentation.cs
@@ -0,0 +1,7 @@
+public class Presentation
+{
+    public string Name { get; set; } = string.Empty;
+    public DateOnly FromDate { get; set; }
+    public DateOnly ToDate { get; set; }
+    public string Location { get; set; } = string.Empty;
+}
